b44631e4bfea974f5b8017702dec634b6978db34
Jose Cepeda <josecepeda@MacBook-Air-de-Jose-6.local>
2026-09-09 22:37:51 -0500

    cambios en servicioDisponiblesEspacio

--- src/main/java/com/example/demo/entities/ServicioDisponiblesEspacio.java
+++ src/main/java/com/example/demo/entities/ServicioDisponiblesEspacio.java
@@ -1,5 +1,6 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,10 +19,11 @@ public class ServicioDisponiblesEspacio {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column (nullable = false)
     private String incluido;
 
     public ServicioDisponiblesEspacio(String incluido){
         this.incluido = incluido;
     }
-    
+
 }
