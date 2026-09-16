8fd9209b78be9b9addfb34206bf13d7b45d58ea3
Jose Cepeda <josecepeda@MacBook-Air-de-Jose-6.local>
2026-09-16 00:27:50 -0500

    avances en reserva controller

--- src/main/java/com/example/demo/controller/ReservaController.java
+++ src/main/java/com/example/demo/controller/ReservaController.java
@@ -1,10 +1,32 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.ReservaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller 
 @RequestMapping ("/reservas")
 public class ReservaController {
+    
+    @Autowired 
+    private ReservaService reservaService;
+
+
+    @GetMapping()
+    public String listarReserva(Model model) {
+        model.addAttribute("reservas", reservaService.findAll());
+        return "listar_reservas";
+    }
+
+    @GetMapping("/crearReserva")
+    public String crearReserva() {
+        return "crear_reserva";
+    }
     
 }
