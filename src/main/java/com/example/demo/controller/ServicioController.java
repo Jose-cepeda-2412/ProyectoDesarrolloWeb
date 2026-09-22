9a61656d8fdc9c6f5afae024c226e8b852747c76
Jose-cepeda-2412 <josecepedav@javeriana.edu.co>
2026-09-22 15:11:11 -0500

    servicio controller completo

--- src/main/java/com/example/demo/controller/ServicioController.java
+++ src/main/java/com/example/demo/controller/ServicioController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -42,11 +44,26 @@ public class ServicioController {
 
     @PostMapping("/guardarServicio")
     public String guardarServicio(@ModelAttribute ("servicio") Servicio servicio) {    
-        servicio.setActivo(true);
-        servicio.setPrecio((Double) servicio.getPrecio());
+        if (servicio.getId() != null) {
+            Servicio datosServicio = servicioService.findById(servicio.getId());
+            servicio.setActivo(datosServicio.getActivo());
+            servicio.setPrecio((Double) servicio.getPrecio());
+        }else{
+            servicio.setActivo(true);
+            servicio.setPrecio((Double) servicio.getPrecio());
+        }
         servicioService.save(servicio);
         return "redirect:/servicios";
     }
+
+    @GetMapping("/modificarServicio/{id}")
+    public String modificarServicio(@PathVariable ("id") Long id, Model model) {
+        Servicio servicio = servicioService.findById(id);
+        model.addAttribute("servicio", servicio);
+        model.addAttribute("espacios", espacioService.findAll());
+        return "crear_servicio";
+    }
+    
     
     @GetMapping("cambiarEstado/{id}")
     public String cambiarEstado(@PathVariable ("id") Long id) {
