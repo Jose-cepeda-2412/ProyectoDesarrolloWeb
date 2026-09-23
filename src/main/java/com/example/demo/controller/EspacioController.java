6424b08b5ff9dcbaf046a75e28d7270f658c5874
Jose-cepeda-2412 <josecepedav@javeriana.edu.co>
2026-09-22 23:23:47 -0500

    cambiar estado de espacios correcto

--- src/main/java/com/example/demo/controller/EspacioController.java
+++ src/main/java/com/example/demo/controller/EspacioController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.service.EspacioService;
@@ -22,5 +23,12 @@ public class EspacioController {
         model.addAttribute("espacios", espacioService.findAll());
         return "listar_espacios";
     }
+
+    @GetMapping("cambiarEstado/{id}")
+    public String getMethodName(@PathVariable ("id") Long id) {
+        espacioService.cambiarEstado(id);
+        return "redirect:/espacios";
+    }
+    
     
 }
