8ecc35699bcc802e62d81766ff0a6e7df9218d79
Jose-cepeda-2412 <josecepedav@javeriana.edu.co>
2026-09-23 00:49:37 -0500

    arreglo en la exception de usuario con id no encontrado

--- src/main/java/com/example/demo/errors/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/errors/GlobalExceptionHandler.java
@@ -1,14 +1,15 @@
 package com.example.demo.errors;
 
-import java.rmi.StubNotFoundException;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice 
 public class GlobalExceptionHandler {
 
     @ExceptionHandler (UsuarioNotFoundException.class)
-    public String handleUserNotFound(StubNotFoundException ex, Model model){
+    public String handleUserNotFound(UsuarioNotFoundException ex, Model model){
         model.addAttribute("mensaje", ex.getMessage());
         return "error";
     }
--- src/main/java/com/example/demo/errors/UsuarioNotFoundException.java
+++ src/main/java/com/example/demo/errors/UsuarioNotFoundException.java
@@ -3,7 +3,7 @@ package com.example.demo.errors;
 public class UsuarioNotFoundException extends RuntimeException{
     
     public UsuarioNotFoundException(Long id){
-        super("No se pudo encontrar el estudiante con id" + id);
+        super("No se pudo encontrar el estudiante con id " + id);
     }
     
 }
